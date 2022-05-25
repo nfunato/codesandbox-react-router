cdc20607956cdbe59bcbf4a543d1b07314ab5d5c
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-25 23:08:53 +0900

    add Query parameter function

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -6,7 +6,9 @@ export const Page2 = () => {
       <h1>Here is Page2</h1>
       {/* In real life, 'to' parameter should be given by JS script */}
       {/* 例えば page2/xxx の数値を書き換えてみるとよい */}
-      <Link to="/page2/555">URL Parameter</Link>
+      <Link to="/page2/999">URL Parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  //const location = useLocation();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
+  // ?name=hogehoge から hogehogeを取得できる
+  // search は ?の後ろ全体
   return (
     <div>
       <h1>Here is UrlParameter Page</h1>
-      <p>Parameter は {id}です</p>
+      <p>URL Parameter は {id}です</p>
+      <p>Query Parameter は　{query.get("name")} です</p>
     </div>
   );
 };
