564e6f71665e83103aca4e49d2eeed53bb8a49c7
Nobuhiko FUNATO <nfunato@acm.org>
2022-05-26 11:56:08 +0900

    add state passing function when page transition

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
   return (
     <div>
       <h1>Here is Page1</h1>
-      <Link to="/page1/detailA">Detail A</Link>
+      {/*　ページ遷移時にpathnameと一緒に状態情報を渡すことができる */}
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>Detail A</Link>
       <br />
+      {/*to に文字列を直接指定するのはオブジェクトを指定する場合の
+         shorthandと考えることができる */}
       <Link to="/page1/detailB">Detail B</Link>
     </div>
   );
